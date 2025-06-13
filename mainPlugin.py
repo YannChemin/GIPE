ab8f1cc084e3a881d7a5d8cabca3a709da9e961b
Yann Chemin <dr.yann.chemin@gmail.com>
2025-06-13 21:30:48 +0000

    Add files via upload

--- mainPlugin.py
+++ mainPlugin.py
@@ -8,30 +8,35 @@ from .about_dialog import AboutDialog # About Dialog class
 class GIPEPlugin:
     def __init__(self, iface):
         self.iface = iface
-        self.menu = '&GIPE'
+        self.menu = None
         self.actions = []
 
     def initGui(self):
+        # Create the custom menu
+        self.menu = QMenu("GIPE", self.iface.mainWindow())
+        self.menu.setObjectName("GIPEMenu")
+
         # Create the action for your dialog
         icon_path = os.path.join(os.path.dirname(__file__), 'icon.png')  # Use your icon
         self.action = QAction(QIcon(icon_path), "Open VI Processing", self.iface.mainWindow())
         self.action.triggered.connect(self.show_dialog)
 
         # Add to GIPE menu (creates menu if not present)
-        self.iface.addPluginToMenu(self.menu, self.action)
+        self.menu.addAction(self.action)
         self.actions.append(self.action)
 
         # Add About to GIPE menu 
         self.about_action = QAction("About...", self.iface.mainWindow())
         self.about_action.triggered.connect(self.show_about_dialog)
-        self.iface.addPluginToMenu(self.menu, self.about_action)
+        self.menu.addAction(self.about_action)
         self.actions.append(self.about_action)
 
+        # Insert the menu into the main menu bar, before the first right standard menu (e.g., "Help")
+        menuBar = self.iface.mainWindow().menuBar()
+        menuBar.insertMenu(self.iface.firstRightStandardMenu().menuAction(), self.menu)
+
     def unload(self):
-        # Remove the action and menu
-        for action in self.actions:
-            self.iface.removePluginMenu(self.menu, action)
-            self.iface.removeToolBarIcon(action)
+        self.menu.deleteLater()
 
     def show_dialog(self):
         dlg = VIProcessing(self.iface.mainWindow())
